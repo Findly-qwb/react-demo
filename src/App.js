d70b0f9f0cb02297ea2d1836a2d654afd2b0579d
邱文斌 <wenbinqiu42@gmail.com>
2021-11-07 14:34:58 +0800

    fix: 测试构建

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
  * @Author:  Findly <weninqiu42@gmail.com>
  * @Date: 2021-11-05 10:52:24
  * @LastEditors: Findly
- * @LastEditTime: 2021-11-07 14:13:52
+ * @LastEditTime: 2021-11-07 14:34:47
  * @Description: 
  */
 import logo from './logo.svg';
@@ -12,7 +12,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>邱文斌大帅比啊啊啊111</h1>
+        <h1>邱文斌大帅比啊啊啊1112222</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
