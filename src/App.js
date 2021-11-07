7973096f470d6b4a0f6922f2ccb4362ee5925170
邱文斌 <wenbinqiu42@gmail.com>
2021-11-07 14:36:58 +0800

    fix:触发webhook

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
  * @Author:  Findly <weninqiu42@gmail.com>
  * @Date: 2021-11-05 10:52:24
  * @LastEditors: Findly
- * @LastEditTime: 2021-11-07 14:34:47
+ * @LastEditTime: 2021-11-07 14:36:47
  * @Description: 
  */
 import logo from './logo.svg';
@@ -13,6 +13,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>邱文斌大帅比啊啊啊1112222</h1>
+        <h2>触发webhook</h2>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
