2c9a86ad15fc47629a62cdda1cf9f7cf57f6bb15
邱文斌 <wenbinqiu42@gmail.com>
2021-11-08 17:46:27 +0800

    fix: react

--- src/App.js
+++ src/App.js
@@ -2,32 +2,21 @@
  * @Author:  Findly <weninqiu42@gmail.com>
  * @Date: 2021-11-05 10:52:24
  * @LastEditors: Findly
- * @LastEditTime: 2021-11-08 17:20:35
- * @Description: 
+ * @LastEditTime: 2021-11-08 17:46:14
+ * @Description:
  */
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>触发了webhook钩子吗？</h2>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<img src={logo} className='App-logo' alt='logo' />
+        <h1>React</h1>
+			</header>
+		</div>
+	);
 }
 
 export default App;
